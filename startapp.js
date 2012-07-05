16a94b245cf5ef44db8bb1ff6a89d46ae444bd70
Dmitry Medvinsky <dmedvinsky@gmail.com>
2012-07-05 19:40:01 +0400

    Remove redundant parens

--- startapp.js
+++ startapp.js
@@ -6,8 +6,8 @@
 var StartApp = function() { };
 
 StartApp.prototype.start = function(params, success, fail) {
-    success = (success) ? success : function() {};
-    fail = (fail) ? fail : function() {};
+    success = success ? success : function() {};
+    fail = fail ? fail : function() {};
     return cordova.exec(success, fail, 'StartApp', 'startApp', [params]);
 };
 
